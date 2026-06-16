599a637d38e402df470d6317cbee94ea954628ef
Farhad Ahmad <farhadahmadhassanzai@gmail.com>
2026-06-16 11:02:12 +0500

    changes baseURL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Create a reusable Axios instance pointing to your Node.js backend
 const API = axios.create({
   //baseURL: "https://bitecheck-api.loca.lt/api", // Make sure this is your current backend URL
-  baseURL: "http://192.168.1.15:5000/api",
+  baseURL: "bytecheck-tau.vercel.app",
 });
 
 // Automatically inject your JWT "digital ID card" token into headers if it exists
